ca8d5b502906ada433fec40f31e27beb950c8423
PaquitoSoft <paquitosoftware@gmail.com>
2021-03-28 16:02:51 +0200

    Added own pilets feed URL

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import { renderInstance } from 'piral';
 import { layout, errors } from './layout';
 
 // change to your feed URL here (either using feed.piral.cloud or your own service)
-const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
+// const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
+const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/paquitosoft_netflix-piral';
 
 renderInstance({
   layout,
@@ -14,3 +15,4 @@ renderInstance({
       .then(res => res.items);
   },
 });
+
